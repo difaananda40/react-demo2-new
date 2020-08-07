e3be19611b7fc9fb6da4cffee0c83a98ed00dd82
difaananda40 <difacool40@gmail.com>
2020-08-07 23:54:45 +0800

    Make tabs unmount on exit

--- src/components/Worksheet/Form/index.jsx
+++ src/components/Worksheet/Form/index.jsx
@@ -134,7 +134,7 @@ const FormContainer = ({
             </div>
           </Modal.Header>
           <Modal.Body style={{ backgroundColor: '#F2F2F2' }}>
-            <Tab.Container id="tab-controlled" activeKey={tabKey} onSelect={(key) => setTabKey(key)}>
+            <Tab.Container id="tab-controlled" activeKey={tabKey} onSelect={(key) => setTabKey(key)} unmountOnExit={true}>
               <Card>
                 <Card.Header>
                   <Nav variant="tabs">
